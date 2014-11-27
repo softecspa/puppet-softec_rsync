a565a303477cbf3ad5da2b9319f75731277d4f63
Lorenzo Cocchi <lorenzo.cocchi@softecspa.it>
2014-11-27 15:25:56 +0100

    no inherits from rsync module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,17 @@
 # == Class: softec_rsync
 #
-# inherits from class rsync.
+# This module manages rsync for sparse file
 #
 # === Authors
 #
 # Author Name <lorenzo.cocchi@softecspa.it>
 #
 
-class softec_rsync inherits rsync {
-  #
+class softec_rsync(
+  $package_ensure = 'present'
+) {
+
+  package { 'rsync':
+    ensure => $package_ensure,
+  } -> Softec_rsync::Get_sparse<| |>
 }
